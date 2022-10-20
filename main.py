cf10de6ba41bca07ed81bd43b52c35cf0282c060
EgorAndreevich17 <theonlyegor1709@gmail.com>
2022-10-20 16:11:45 +0300

    changed the architecture of code

--- main.py
+++ main.py
@@ -1,21 +1,28 @@
 import pygame, sys
 
-pygame.init()
-screen = pygame.display.set_mode((1024, 768))
-pygame.display.set_caption('Test Game')
-# screen.fill('#FB607F')
-clock = pygame.time.Clock()
+class Game:
+    def __init__(self):
 
-background_surface = pygame.image.load('graphics/background.png')
-grass_surface = pygame.image.load('graphics/grass.png')
+        #general group
+        pygame.init()
+        self.screen = pygame.display.set_mode((1024, 768))
+        pygame.display.set_caption('Test Game')
+        self.clock = pygame.time.Clock()
+    def run(self):
+        background_surface = pygame.image.load('graphics/background.png')
+        grass_surface = pygame.image.load('graphics/grass.png')
+        while True:
+            pygame.display.update()
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+            self.screen.blit(background_surface, (0, 0))
+            self.screen.blit(grass_surface, (0, 0))
+            self.clock.tick(60)
 
-while True:
-    pygame.display.update()
-    for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-    #screen.blit(grass_surface,(0,0))
-    screen.blit(background_surface,(0,0))
-    screen.blit(grass_surface,(0,0))
-    clock.tick(60)
+
+if __name__=="__main__":
+    game = Game()
+    game.run()
+
